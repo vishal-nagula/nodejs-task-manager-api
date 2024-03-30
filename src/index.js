3fc759ea8ddaefe80ec5d04d9d79000ba851125f
Your Name <you@example.com>
2024-03-30 13:08:19 +0530

    index.js added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-//comments
+const express = require('express');
+const bodyParser = require('body-parser');
+const taskManagerRouters = require('./routes/tastManagerRoutes');
+
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+/*USING ROUTER MODULE FOR ALL ROUTES*/
+console.log("here added")
+app.use(taskManagerRouters);
+
+/*TEST SERVER STATUS*/
+app.get("/ping",(req,res)=>{
+    res.send("PONG...!!!!");
+});
+
+
+/*SERVER START UP*/
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+   
+}
+)
